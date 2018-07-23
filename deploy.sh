5160b800ad591a644b336bdaa0a89f1cf13f7d91
とーふとふ <tohu.soy@gmail.com>
2018-07-23 18:25:08 +0900

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 docker build -t vuepress-build docker/node
-docker run --rm -v `pwd`:/work -w /work -t vuepress-build 'npm i && npm run build'
+docker run --rm -v `pwd`:/work -w /work -t vuepress-build /bin/bash -c 'npm i && npm run build'
 
 docker-compose down
-docker-compose up
+docker-compose up -d
