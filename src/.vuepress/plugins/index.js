56eef0a3e83c3aeb405bafcc1d5d5d50c62e8bb0
tohutohu <tohu.soy@gmail.com>
2019-01-03 12:40:55 +0900

    fix

--- src/.vuepress/plugins/index.js
+++ src/.vuepress/plugins/index.js
@@ -5,7 +5,7 @@ module.exports = (options, ctx) => {
   return {
     extendPageData ({_strippedContent, frontmatter, _filePath, _context: { markdown },  ...rest}) { 
       if (typeof _strippedContent !== 'string') return
-      const slug = path.basename(_filePath, false)
+      const slug = path.basename(_filePath, '.md')
       const pagePath = getPermalink({pattern: frontmatter.permalink, date: frontmatter.date, slug: slug})
       const {data: {__data_block: data}} = markdown.render(_strippedContent)
       if (data && data.images) {
