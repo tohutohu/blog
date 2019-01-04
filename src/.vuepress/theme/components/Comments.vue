248fe0838a450d90f4208bb3b1b88d0d461d13fa
tohutohu <tohu.soy@gmail.com>
2019-01-04 20:04:09 +0900

    send comment

--- src/.vuepress/theme/components/Comments.vue
+++ src/.vuepress/theme/components/Comments.vue
@@ -15,21 +15,22 @@
       <summary>
         <span class="send-comment">コメントする</span>
       </summary>
-      <form action="post" netlify>
+      <form name="suspendedComments" method="post" netlify netlify-honeypod="bot-field">
         <div class="form-name">
-          <div class="label" for="name">Name: 
-          </div>
-            <input type="text" name="name" id="">
+          <div class="label" for="name">Name:</div>
+          <input type="text" name="name" id="">
         </div>
         <div class="form-email">
-          <div class="label" for="email">E-mail: </div>
-            <input type="email" name="email" id="">
+          <div class="label" for="email">E-mail:</div>
+          <input type="email" name="email" id="">
         </div>
         <div class="form-content">
-        <div class="label" for="content">Content:</div>
+          <div class="label" for="content">Content:</div>
           <textarea name="content" id="" cols="30" rows="10"></textarea>
         </div>
-        <input type="text" :value="$page.key" hidden>
+        <input type="hidden" name="pageKey" :value="$page.key">
+        <input type="hidden" name="form-name" value="suspendedComments">
+        <div>コメントは承認されると表示されます</div>
         <input type="submit" value="Send">
       </form>
     </details>
